fa2f2c752bbf6e73cf49b3548f8138350a4b1ca1
David Wu  (Qingliao Wu) <41318040+qingliaowu@users.noreply.github.com>
2022-08-20 10:57:52 +0000

    update

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
+cats = {"k":1,"a":2}
 
-import random
+cats["b"] = 10
 
-a = random.random()
-
-print(a)
-print("Hello World")
-
-
-day = 21
-month = 'Oct'
-temp = 65
-
-x = f"Today is {day}"
-
-print (x)
-
-
-a = True
-
-a = False
-
-if a:
-  print("ok")
-else:
-  print("ng")
-
-
-print("------------------")
-
-
-a = [3,4]
-length = len(a)
-
-print(length)
-a.append(5)
-b = a
-print(b)
-
-del(b[0])
-
-for number in range(10):
-  print (number, end =" ")
+print(cats)
+print(cats["b"])
